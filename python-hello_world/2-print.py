f6ec8982cd3b8a8df5b78831dc4a87cb2502c49c
d404cjebq <imdhaydhw@gmail.com>
2026-06-01 17:53:15 +0300

    Add script to print introductory quote

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 # Print a string with an opening quotation mark
-print ("\"Programming is like building a multilingual puzzle")
+print("\"Programming is like building a multilingual puzzle")
