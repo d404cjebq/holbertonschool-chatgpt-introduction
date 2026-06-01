d6cd27a8507b4ece20e7b1e11b1ba4db303433d6
d404cjebq <imdhaydhw@gmail.com>
2026-06-01 16:35:57 +0300

    fix: implement game win detection for revealed safe cells

---
+++ debugging/mines.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+import os
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+class Minesweeper:
+    def __init__(self, width=10, height=10, mines=10):
+        self.width =
