0c49718b24834074acedc3a1e349302cb7d8c1ef
d404cjebq <imdhaydhw@gmail.com>
2026-06-02 17:00:03 +0300

    Solve 8-concat_edges.py using string slicing

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# tring slicing
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[:6])
